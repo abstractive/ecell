a6357f787b2f5d31754744960a6f1e62d75c117c
benzrf <benzrf@benzrf.com>
2016-06-13 13:40:15 -0400

    Migrated `spool.rb`.

---
+++ lib/ecell/base/shapes/spool.rb
@@ -0,0 +1,72 @@
+require 'ecell'
+require 'ecell/elements/figure'
+
+#benzrf TODO: maybe try to capture cross-actor order?
+module ECell
+  module Base
+    module Shapes
+      class Spool < ECell::Elements::Figure
+        def initialize
+          #benzrf TODO: proper require for this
+          ECell::Logger.debug("Initialized Spooling Backlogger") if DEBUG_DEEP
+          @backlog = {}
+          @timers = {}
+        end
+
+        def [](figure_id)
+          @backlog[figure_id] ||= Backlog.new(figure_id)
+          forward(figure_id)
+          @backlog[figure_id]
+        end
+
+        def forward(figure_id)
+          @timers[figure_id] ||= after(1) {
+            if alive?(figure_id)
+              @backlog[figure_id].each { |command|
+                ECell.sync(figure_id).async(command[:method], *command[:args])
+              }
+              @backlog[figure_id].flush
+              @timers[figure_id].cancel rescue nil
+              @timers[figure_id] = nil
+            else
+              forward(figure_id)
+            end
+          }
+        end
+
+        def alive?(figure_id)
+          ECell.sync(figure_id) && ECell.sync(figure_id).alive?
+        rescue
+          false
+        end
+
+        class Backlog
+          attr_reader :commands
+
+          def initialize(figure_id)
+            @commands = []
+            @figure_id = figure_id
+          end
+
+          def method_missing(method,*args)
+            @commands << { method: method, args: args.dup }
+          end
+
+          def flush
+            @commands = []
+          end
+
+          def each(&block)
+            @commands.each(&block)
+          end
+        rescue => ex
+          raise ECell::Logger.exception(ex, "Backlog Exception")
+        end
+
+      rescue => ex
+        raise ECell::Logger.exception(ex, "Spool Exception")
+      end
+    end
+  end
+end
+
