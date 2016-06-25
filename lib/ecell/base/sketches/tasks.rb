bc5b35e6db73e1ea6113a8845bcaee1b45af8b59
benzrf <benzrf@benzrf.com>
2016-06-24 22:40:43 -0400

    Migrated `service/tasks.rb`, `service/events.rb`, `tasks/**/*.rb`, and `events/**/*.rb`.

---
+++ lib/ecell/base/sketches/tasks.rb
@@ -0,0 +1,45 @@
+require 'ecell/elements/subject'
+require 'ecell/base/designs/follower'
+require 'ecell/base/designs/worker'
+require 'ecell'
+
+module ECell
+  module Base
+    module Sketches
+      class Tasks < ECell::Elements::Subject
+        def initialize(configuration={})
+          design! ECell::Base::Designs::Follower,
+                  ECell::Base::Designs::Worker
+          super(configuration)
+        rescue => ex
+          raise exception(ex, "Failure initializing.")
+        end
+
+        def at_provisioning
+          super {
+            endpoint = ECell.sync(:distribution).distribution_input!(:tasks)
+            line! :distribution_pull, mode: :connecting, endpoint: endpoint
+          }
+        end
+
+        module Operations
+          def work_10
+            #de Send back time estimate, then do real work.
+          end
+
+          def work_9; end
+          def work_8; end
+          def work_7; end
+          def work_6; end
+          def work_5; end
+          def work_4; end
+          def work_3; end
+          def work_2; end
+          def work_1; end
+        end
+        include Operations
+      end
+    end
+  end
+end
+
