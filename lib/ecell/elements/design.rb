01c9651830a527a2c2a765fc461eb273c7bc926f
benzrf <benzrf@benzrf.com>
2016-07-19 13:03:11 -0400

    Added more details to the `Design` documentation.

--- lib/ecell/elements/design.rb
+++ lib/ecell/elements/design.rb
@@ -43,16 +43,55 @@ module ECell
         }
       ]
 
-      # {Injections} is a hash specifying several things, including:
-      # * TODO
-      Injections = {}
+      # There are 4 kinds of "injections":
+      #
+      # * Emitters - attaching on-message callbacks to Lines.
+      # * Relayers - automatically relaying messages from one Line to another.
+      # * Events - registering callbacks for certain kinds of events.
+      # * Executives - methods to run at specified FSM states. Can be sync or async.
+      #
+      # {Injections} is a hash specifying the injections added by the Design.
+      Injections = {
+        emitters: {
+          fsm_state_to_start_emitting_at: [
+            [:line_id, :method_on_subject],
+            [:line_id2, :figure_id, :method_on_figure]
+          ]
+        },
+        relayers: {
+          relayer_figure_id: [
+            [:source_line_id, :target_line_id]
+          ]
+        },
+        events: {
+          event_id: [
+            :method_on_subject
+          ]
+        },
+        executive_sync: {
+          some_fsm_state: [
+            :method_on_subject,
+            [:method_on_subject, ["some", "args", 42]],
+            [:figure_id, :method_on_figure],
+            [:figure_id, :method_on_figure, ["some", "args", 42]]
+          ]
+        },
+        executive_async: {
+          # same format as executive_sync
+        }
+      }
       # {Disabled} will optionally indicate injections to disable that might
-      # otherwise be present from other Designs.
-      Disabled = {}
+      # otherwise be added by other Designs.
+      Disabled = {
+        # same format as Injections
+      }
 
       # If the {Methods} module exists in a Design, it will be included into
-      # any Sketches that use the design.
+      # any Sketches that use the Design.
       module Methods
+        def method_on_subject(*args)
+          debug("method_on_subject called with: #{args}")
+        end
       end
     end
   end
